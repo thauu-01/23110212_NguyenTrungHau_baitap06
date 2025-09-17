726b3a1312f9057e5be1197a272c1160c7b532db
thauu-01 <popola957th@gmail.com>
2025-09-17 22:28:17 +0700

    Upload code

--- baitap06_springboot/src/main/java/com/example/demo/controller/CategoryController.java
+++ baitap06_springboot/src/main/java/com/example/demo/controller/CategoryController.java
@@ -5,17 +5,12 @@ import com.example.demo.entity.User;
 import com.example.demo.service.CategoryService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/category")
